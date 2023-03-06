23e92a0d425242f236157724a00332bb11c1ce91
hardik3rddigital <hardik.s@3rddigital.com>
2023-03-06 11:51:14 +0530

    React Hook Form - register,handleSubmit,error - register object and array

--- src/SimpleForm.js
+++ src/SimpleForm.js
@@ -9,6 +9,9 @@ function SimpleForm() {
       phone: "",
       password: "",
       gender: "",
+      selectedstate: "",
+      agree: "",
+      sendme: "",
     },
   });
 
@@ -93,6 +96,74 @@ function SimpleForm() {
                 />
                 <label className="form-check-label" htmlFor="other">
                   other
+                </label>
+              </div>
+            </div>
+            <div className="form-group mb-3">
+              <select
+                className="custom-select form-select"
+                defaultValue=""
+                {...register("selectedstate")}
+              >
+                <option value="">Select your State</option>
+                <option value="Gujarat">Gujarat</option>
+                <option value="Delhi">Delhi</option>
+                <option value="Punjab">Punjab</option>
+                <option value="Jharkhand">Jharkhand</option>
+                <option value="Bihar">Bihar</option>
+              </select>
+            </div>
+
+            <div className="form-group mb-3">
+              <select
+                className="custom-select form-select"
+                defaultValue=""
+                {...register("state.city")}
+              >
+                <option value="">Select your City</option>
+                <option value="Surat">Surat</option>
+                <option value="Ahmedabad">Ahmedabad</option>
+                <option value="Baroda">Baroda</option>
+                <option value="Gandhinagar">Gandhinagar</option>
+              </select>
+            </div>
+
+            <div className="form-group mb-3">
+              <select
+                className="custom-select form-select"
+                defaultValue=""
+                {...register("state.area")}
+              >
+                <option value="">Select your Area</option>
+                <option value="Bapunagar">Bapunagar</option>
+                <option value="Viratnagar">Viratnagar</option>
+                <option value="Nikol">Nikol</option>
+                <option value="Naroda">Naroda</option>
+              </select>
+            </div>
+
+            <div className="form-group mb-3">
+              <div className="form-check form-check-inline">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="terms"
+                  defaultValue="agree"
+                  {...register("agree", {})}
+                />
+                <label className="form-check-label" htmlFor="terms">
+                  I agree all terms and conditons
+                </label>
+              </div>
+              <div className="form-check form-check-inline">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id="updates"
+                  {...register("sendMe", {})}
+                />
+                <label className="form-check-label" htmlFor="updates">
+                  Send me latest Bootstrap updates
                 </label>
               </div>
             </div>
